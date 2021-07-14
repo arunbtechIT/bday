368a24bb5ac61ada82262429fd8d3d5b36572657
arunbtechIT <64957846+arunbtechIT@users.noreply.github.com>
2021-07-14 22:13:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "swapna"&&b=="akka
+")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
